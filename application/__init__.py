df4e0f74431ccf4c0675a2115e10ea4fd37458c5
Virva <virva.svala@gmail.com>
2018-08-02 18:03:53 +0300

    VKO3 kirjautuminen

--- application/__init__.py
+++ application/__init__.py
@@ -14,11 +14,34 @@ app.config["SQLALCHEMY_ECHO"] = True
 # Luodaan db-olio, jota käytetään tietokannan käsittelyyn
 db = SQLAlchemy(app)
 
+# oman sovelluksen toiminnallisuudet
 # Luetaan kansiosta application tiedoston views sisältö
 from application import views
 
 from application.runot import models
 from application.runot import views
+
+from application.auth import models 
+from application.auth import views
+
+# kirjautuminen
+from application.auth.models import User
+from os import urandom
+app.config["SECRET_KEY"] = urandom(32)
+
+from flask_login import LoginManager
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+login_manager.login_view = "auth_login"
+login_manager.login_message = "Please login to use this functionality."
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+
+
 
 # Luodaan lopulta tarvittavat tietokantataulut
 db.create_all()
